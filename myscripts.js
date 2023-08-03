3b65afc0750ff6c5f004f43f62a70d30cbbef215
subhadip-kundu <subhadipkundu106@gmail.com>
2023-08-03 22:06:39 +0530

    Chapter 1 complete

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+// <!-- Setup and start animation! -->
+
+const typed = new Typed('#element', {
+    strings: ['Fresher Front End Developer'],
+    typeSpeed: 70,
+    backSpeed: 90,
+    showCursor: true,
+    cursorChar: '|',
+    loop: true
+  });
+
+
+AOS.init();
